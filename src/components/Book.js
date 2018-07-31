f62373da790315a186ea0486685bd149aa37a410
Kerstin Wagner <kerstin-wagner@gmx.at>
2018-07-31 13:05:49 +0200

    feat: Displaying props data of 2 books

--- src/components/Book.js
+++ src/components/Book.js
@@ -6,8 +6,18 @@ class Books extends Component {
 		return (
 		<ol className="book-list">
 		{this.props.bookTitles.map((book) => (
-			<li key={book.id}>
-			{book.title}
+			<li key={book.id} className=".list-books-title">
+			<div className='book-cover' style={{
+              backgroundImage: `url(${book.cover})`,
+            }}/>
+			<div className='list-books-content'>
+              <p>{book.title}</p>
+              <p>{book.authors}</p>
+            </div>
+            <button className='open-search'>
+              Search
+            </button>
+			
 			</li>
 			))}
 		</ol>
