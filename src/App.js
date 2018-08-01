a0cb3c20efdeb87bcf582c1ce2437f2cd34b5309
Kerstin Wagner <kerstin-wagner@gmx.at>
2018-08-01 17:25:26 +0200

    feat: Receive data from BookAPI

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 import ReactDom from 'react-dom'
 import Books from './components/Book'
@@ -9,24 +9,17 @@ import SearchBar from './components/Search'
 
 class BookApp extends React.Component {
 	state = {
-		bookTitles: [
-  {
-    "id": "Ender's Game",
-    "title": "Ender's Game",
-    "authors": "Orson Scott Card",
-    "cover": "http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api"
-  },
-  {
-    "id": "Harry Potter and the Sorcerer's Stone",
-    "title": "Harry Potter and the Sorcerer's Stone",
-    "authors": "J.K. Rowling",
-    "cover": "http://books.google.com/books/content?id=pD6arNyKyi8C&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE70Rw0CCwNZh0SsYpQTkMbvz23npqWeUoJvVbi_gXla2m2ie_ReMWPl0xoU8Quy9fk0Zhb3szmwe8cTe4k7DAbfQ45FEzr9T7Lk0XhVpEPBvwUAztOBJ6Y0QPZylo4VbB7K5iRSk&source=gbs_api"
-  }
-]
-		}
+		bookTitles: []
+	}
+
+componentDidMount() {
+	BooksAPI.getAll().then((book)=> {
+		this.setState({bookTitles: book})
+	})
+}
 		
 
-
+//User clicks button to change book shelves
 	changeShelf = (book) => {
 		this.setState((state) => ({
 			bookTitles: state.bookTitles.filter((b) => b.id !== book.id)
